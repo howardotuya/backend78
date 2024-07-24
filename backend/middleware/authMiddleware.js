fed88f6b3de9241f2f3603855f254e8764b4d0b6
Howard Otuya <howardotuya@yahoo.com>
2024-07-24 19:05:13 +0100

    updated middleware

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -3,13 +3,18 @@ import User from "../models/userModel.js";
 import expressAsyncHandler from "express-async-handler";
 
 const protect = expressAsyncHandler(async (req, res, next) => {
-  let token;
-  token = req.cookies.jwt;
+  let token = req.cookies.jwt;
 
   if (token) {
     try {
-      const decodedToken = jwt.decode(token, process.env.JWT_SECRET);
+      const decodedToken = jwt.verify(token, process.env.JWT_SECRET); // Use verify instead of decode
       req.user = await User.findById(decodedToken.userId).select("-password");
+
+      if (!req.user) {
+        res.status(401);
+        throw new Error("Unauthorized, user not found");
+      }
+
       next();
     } catch (error) {
       res.status(401);
